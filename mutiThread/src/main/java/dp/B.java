5e1cbff82eb4fa028e2bedc3bb592ade2fb18e5c
陈杰 <chenj@chinatelecom.cn>
2017-09-23 21:53:12 +0800

    多线程+父子类的构造的执行顺序

---
+++ mutiThread/src/main/java/dp/B.java
@@ -0,0 +1,19 @@
+package dp;
+
+/**
+ * ${DESCRIPTION}
+ *
+ * @author: chenjie
+ * @date: 2017/9/21
+ */
+public class B extends A {
+
+	public B(int age) {
+//		super(age);
+		System.out.println("test");
+	}
+
+	public static void main(String[] args) {
+		new B(123);
+	}
+}
