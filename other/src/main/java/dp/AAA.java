d1efedf778ee3cd30900c8aad8ceb23838762598
陈杰 <chenj@chinatelecom.cn>
2017-09-27 21:40:29 +0800

    unknown

---
+++ other/src/main/java/dp/AAA.java
@@ -0,0 +1,24 @@
+package dp;
+
+/**
+ * ${DESCRIPTION}
+ *
+ * @author: chenjie
+ * @date: 2017/9/24
+ */
+public class AAA {
+
+	public AAA(int age) {
+		this.age = age;
+	}
+
+	private int age;
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
