5dd9fd53203a6e8deace262408b204f60160d7fe
陈杰 <chenj@chinatelecom.cn>
2017-11-08 14:09:59 +0800

    GC

---
+++ jvm/src/main/java/dp/MinorGC.java
@@ -0,0 +1,21 @@
+package dp;
+
+/**
+ *
+ * @author chenjie
+ * @create 2017-10-17 14:51
+ */
+public class MinorGC {
+
+    private static final int _1MB = 1024 * 1024;
+
+    public static void main(String[] args) {
+        byte[] a1,a2,a3,a4;
+        a1 = new byte[2 * _1MB];
+        a2 = new byte[2 * _1MB];
+        a3 = new byte[2 * _1MB];
+        a4 = new byte[4 * _1MB];
+
+    }
+}
+
